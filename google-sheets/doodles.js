beb7e91423ff353f020ec3850f9d5ce3ea2e3777
Marie Treschow <marie.treschow@mobgen.com>
2018-08-22 16:02:45 +0200

    add Procfile

--- google-sheets/doodles.js
+++ google-sheets/doodles.js
@@ -1,13 +1,13 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const _ = require('lodash')
 
-const doc = new GoogleSpreadsheet('1Fm16jiOYLe0KfbQA5WQmVYqSGuu5OuiOA66q3h09cHE');
+const doc = new GoogleSpreadsheet('13ET2fjPPKrmZ1XSptQK45EHZmq1hbs218PCUlt6zERc');
 
 function get () {
     return new Promise((resolve, reject) => {
         return doc.getRows(1, (err, rows) => {
             const doodles = _.map(rows, (row) => {
-                return row.fact
+                return row.doodles
             })
             const item = doodles[Math.floor(Math.random()*doodles.length)];
             resolve(item)
